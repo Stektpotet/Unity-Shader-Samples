31e8c99da59a30f267aa66f4aedeec0faffb428d
Halvor <stektpotet@gmail.com>
2019-03-25 10:57:43 +0100

    Initial commit

---
+++ Assets/MoveBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBehaviour : MonoBehaviour
+{
+    public void Move(Vector3 velocity)
+    {
+        transform.position += velocity;
+    }
+
+}
